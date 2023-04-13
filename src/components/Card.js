f0e9026099e16bcc0a09e737b62bc332d25090e0
Elyse Newland <elysenewland@gmail.com>
2023-04-13 18:11:24 -0400

    User info on cards updated to include phone and city

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 // Create card for each robot friend using image from robohash.org
-const Card = ({name, email, id}) => {
+const Card = ({name, email, phone, city, id}) => {
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="A cute robot." src={`https://robohash.org/${id}?}200x200`} />
+            <img alt={name + "'s Profile"} src={`https://robohash.org/${id}?}200x200`} />
             <div>
                  <h2>{name}</h2>
-                 <p>{email}</p>
+                 <ul className="list">
+                    <li>Email: {email}</li>
+                    <li>Phone: {phone}</li>
+                    <li>City: {city}</li>
+                 </ul>
             </div>       
         </div>
     );
